ca2ebc1f62e0026a29886df518c02d987f48911e
garasyo <igara1119@gmail.com>
2020-01-12 15:31:30 +0900

    modify: notification list style.

--- src/components/notifications_view/index.style.ts
+++ src/components/notifications_view/index.style.ts
@@ -10,21 +10,25 @@ export const tableRowView = css`
 `.toString();
 
 export const tableTitleView = css`
+  flex-direction: column;
   width: 150px;
   padding: 2px;
 `.toString();
 
 export const tableSubTitleView = css`
+  flex-direction: column;
   width: 150px;
   padding: 2px;
 `.toString();
 
 export const tableBodyView = css`
+  flex-direction: column;
   width: 250px;
   padding: 2px;
 `.toString();
 
 export const tableDateView = css`
+  flex-direction: column;
   width: 150px;
   padding: 2px;
 `.toString();
